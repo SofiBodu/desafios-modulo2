210572e5aa4ca5e62f9258296224d64c4e80f7d3
SofiBodu <bodusofi@gmail.com>
2024-09-21 12:52:23 -0300

    agregue el JavaScript y cambie cosas minimas del HTML

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let nombreProducto = "Brownie"; 
+let precioUnitario = 500; 
+let cantidadDeseada;
+
+cantidadDeseada = prompt("¿Cuántos brownies desea comprar?");
+
+let costoTotal = precioUnitario * cantidadDeseada;
+
+
+alert("El costo total de tu compra es: $" + costoTotal);
