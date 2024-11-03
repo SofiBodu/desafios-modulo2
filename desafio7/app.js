87291f82416d3c15524fe4f93acb55a09dce47af
SofiBodu <bodusofi@gmail.com>
2024-11-03 18:59:49 -0300

    .

---
+++ desafio7/app.js
@@ -0,0 +1,6 @@
+const precio = document.getElementById("precioProducto");
+const estilo = document.querySelector("p");
+
+precio.innerText = "precio = $450";
+estilo.style.color="#ff8a8a"
+estilo.style.fontWeight="800"
